44b65fd054de60bf9da1f551334cdfaa23f279fb
Nabucodonozor <RockStoneStudios666@gmail.com>
2020-11-27 17:48:36 -0500

    Despliegue Final...

--- js/darkmode.js
+++ js/darkmode.js
@@ -16,21 +16,21 @@ document.getElementById("mode-dark").addEventListener("click", function () { // 
 
 function seteoDark() {
   if (localStorage.getItem("dark-mode") === "true") {// aca pregunto en que modo estoy en este caso si es nocturno 
-    document.getElementById("logo").setAttribute("src", "/assets/logo-mobile-modo-noct.svg"); // cambio icono
+    document.getElementById("logo").setAttribute("src", "./assets/logo-mobile-modo-noct.svg"); // cambio icono
     document.getElementById("srch-btn").style.color = "#FFFFFF"; //cambio boton-buscar a blanco
     document.getElementById("mode-dark").textContent = "Modo Diurno"; // cambio el texto de boton a modo diurno
     document.getElementById("cuerpo").classList.add("cuerpo-dark"); // cambio todo el body a nocturno
     document.getElementById("cuerpo").classList.remove("cuerpo"); //remuevo la clase del modo diurno
-    document.getElementById("camara").setAttribute("src", "/assets/camara-modo-noc.svg"); // cambio imagen camara
-    document.getElementById("pelicula").setAttribute("src", "/assets/pelicula-modo-noc.svg");//cambio imagen
+    document.getElementById("camara").setAttribute("src", "./assets/camara-modo-noc.svg"); // cambio imagen camara
+    document.getElementById("pelicula").setAttribute("src", "./assets/pelicula-modo-noc.svg");//cambio imagen
   } else {
      // por defecto si no estoy en modo nocturno 
-    document.getElementById("logo").setAttribute("src", "/assets/logo-mobile.svg"); //dejo todo en modo original
+    document.getElementById("logo").setAttribute("src", "./assets/logo-mobile.svg"); //dejo todo en modo original
     document.getElementById("srch-btn").style.color = "#572EE5";
     document.getElementById("mode-dark").textContent = "Modo Nocturno";
     document.getElementById("cuerpo").classList.remove("cuerpo-dark");
     document.getElementById("cuerpo").classList.add("cuerpo");
-    document.getElementById("camara").setAttribute("src", "/assets/camara.svg");
-    document.getElementById("pelicula").setAttribute("src", "/assets/pelicula.svg");
+    document.getElementById("camara").setAttribute("src", "./assets/camara.svg");
+    document.getElementById("pelicula").setAttribute("src", "./assets/pelicula.svg");
   }
 }
